f1c397e89dd198250c3ee2f756772d479b869247
ashimdhungana07 <ashimdhungana6@gmail.com>
2024-08-07 16:55:57 +0545

    new

--- CWH Course/11) Advanced Features/exception.py
+++ CWH Course/11) Advanced Features/exception.py
@@ -1,9 +1,22 @@
-try:
-    a = int(input("Hey, Enter a number: "))
-    print(a)
+def main():
+    try:
+        a = int(input("Hey, Enter a number: "))
+        print(a)
 
-except ValueError as v:
-    print(v)
+    except Exception as e:
+        print(e)
 
-except Exception as e:
-    print(e)
+    finally:
+        print("This runs no matter the outcome.")
+
+main()
+
+
+#RAISING EXCEPTION
+
+n1 = int(input("\nEnter number to be divided: "))
+n2 = int(input("Enter another number: "))
+if (n2==0):
+    raise ZeroDivisionError("You cannot divide by zero")
+else:
+    print(n1/n2)
---
+++ CWH Course/11) Advanced Features/module.py
@@ -0,0 +1,8 @@
+def myFunc():
+    print("Hello world!")
+
+myFunc()
+print(__name__)
+
+if __name__ =="__main__":
+    print("We are directly running this code from main file.")
