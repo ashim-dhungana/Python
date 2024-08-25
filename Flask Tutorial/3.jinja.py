39759f018958116392f2bd5332e460236b80e887
ashimdhungana07 <ashimdhungana6@gmail.com>
2024-08-25 12:27:51 +0545

    new

--- Flask Tutorial/3.jinja.py
+++ Flask Tutorial/3.jinja.py
@@ -3,6 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template('jinja.html')
+    return render_template('index2.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+@app.route("/post")
+def post():
+    return render_template('post.html')
+
+@app.route("/image")
+def image():
+    return render_template('image.html')
 
 app.run(debug=True)
