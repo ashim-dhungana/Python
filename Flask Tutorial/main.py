b7bcbb209e1eb8030d77f02ea769e203a94d55d2
ashimdhungana07 <ashimdhungana6@gmail.com>
2024-08-25 17:26:59 +0545

    new

--- Flask Tutorial/main.py
+++ Flask Tutorial/main.py
@@ -5,19 +5,19 @@ app = Flask(__name__)
 def main():
     return render_template('index2.html')
 
-@app.route("/about")
+@app.route("/about.html")
 def about():
     return render_template('about.html')
 
-@app.route("/contact")
+@app.route("/contact.html")
 def contact():
     return render_template('contact.html')
 
-@app.route("/post")
+@app.route("/post.html")
 def post():
     return render_template('post.html')
 
-@app.route("/image")
+@app.route("/image.html")
 def image():
     return render_template('image.html')
 
